2d413163dab7c355ca2408016e33b3b297749cc3
MuhammadBilalYousaf <bilalyousaf629422@gmail.com>
2024-10-24 01:49:55 +0500

    A image gellery app designed with html, css and javascript to show images in unique style. this is codealpha intership project which is completed.

---
+++ script.js
@@ -0,0 +1,21 @@
+// Select the file input and gallery container
+const fileInput = document.getElementById('file-input');
+const uploadedGallery = document.getElementById('uploaded-gallery');
+
+fileInput.addEventListener('change', function(event) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const imgElement = document.createElement('img');
+            imgElement.src = e.target.result;
+            imgElement.alt = file.name;
+            imgElement.classList.add('uploaded-image');
+            uploadedGallery.appendChild(imgElement);
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
